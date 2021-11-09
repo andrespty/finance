6881a1a96002a932f446b4523209885e86aea997
andrespty <andresholee@gmail.com>
2021-11-09 18:30:27 -0500

    Safe area

--- finance/src/App.js
+++ finance/src/App.js
@@ -3,7 +3,7 @@ import Homepage from './views/Homepage/Homepage';
 
 function App() {
   return (
-    <Box>
+    <Box paddingTop='env(safe-area-inset-top)'>
 
     <Center>
       <Heading>Finance</Heading>
