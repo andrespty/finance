09030281f88c97f623cb9034ec5210a20ddd9d20
andrespty <andresholee@gmail.com>
2021-11-09 18:42:24 -0500

    HTML Changes

--- finance/src/App.js
+++ finance/src/App.js
@@ -3,7 +3,7 @@ import Homepage from './views/Homepage/Homepage';
 
 function App() {
   return (
-    <Box >
+    <Box style={{padding: 'env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left)'}}>
 
     <Center>
       <Heading>Finance</Heading>
