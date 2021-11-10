3caa6b831d5b0b32dd6edbfffc01f6ce012f7783
andrespty <andresholee@gmail.com>
2021-11-09 22:20:13 -0500

    Small changes

--- finance/src/App.js
+++ finance/src/App.js
@@ -1,15 +1,25 @@
-import { Box, Center, Heading } from '@chakra-ui/layout';
+import { Box, Center, Heading, ButtonGroup, Button, Divider, Stack } from '@chakra-ui/react';
 import Homepage from './views/Homepage/Homepage';
 import CompoundingPage from './views/Compounding_Interest/CompoundingPage';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 function App() {
   return (
     <Box h='100vh'>
 
-    <Center>
-      <Heading>Finance</Heading>
+    <Center m={3} >
+      <Stack direction='column' w='40%'>
+        <Heading align='center'>Finance</Heading>
+        <Divider />
+      </Stack>
     </Center>
 
+    <Center>
+      <ButtonGroup variant='ghost' size='sm'>
+        <Link to='/'><Button>Split</Button></Link>
+        <Link to='/compounding'><Button>Compounding</Button></Link>
+      </ButtonGroup>
+    </Center>
+    
     <Routes>
 
       <Route path='compounding' element={<CompoundingPage />} /> 
